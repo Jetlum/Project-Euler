000f8f482764ff2e955a41cb2a727296ffbe61ed
Jetlum <jetlumajeti@gmail.com>
2021-02-08 18:36:14 +0100

    Beauify output display

--- specialpythagoreantriplet.go
+++ specialpythagoreantriplet.go
@@ -10,7 +10,7 @@ func main() {
 		for b := 1.0; b <= 1000; b++ {
 			for c := 1.0; c <= 1000; c++ {
 				if a+b+c == 1000 && (math.Pow(a, 2)+math.Pow(b, 2) == math.Pow(c, 2)) {
-					fmt.Printf("a is %v, b is %v, c is %v:", a, b, c)
+					fmt.Printf("a is %v, b is %v, c is %v, a*b*c=", a, b, c)
 					fmt.Println(a * b * c)
 				}
 			}
